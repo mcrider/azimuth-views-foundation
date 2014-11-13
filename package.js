b04743163813219d17abeb8b0c83412e449d8e29
Matt Crider <mattcrider@gmail.com>
2014-11-12 21:16:44 -0500

    Fixes for meteor 1.0

--- package.js
+++ package.js
@@ -1,52 +1,52 @@
 Package.describe({
-    summary: "Azimuth CMS frontend templates (using Foundation 5.x)"
+  name: 'mcrider:azimuth-views-foundation',
+  summary: 'Azimuth CMS frontend templates (using Foundation 5.x)',
+  version: '0.3.7',
+  git: 'https://github.com/mcrider/azimuth-views-foundation'
 });
 
-Package.on_use(function (api) {
-  api.use(['less', 'templating', 'modernizr-meteor'], 'client');
-  api.use(['azimuth-core'], 'client', {unordered: true});
+Package.onUse(function (api) {
+  api.use(['less', 'templating', 'mrt:modernizr-meteor', 'jquery'], 'client');
+  api.use('mcrider:azimuth-core', {unordered: true});
 
-  api.add_files([
-    'lib/foundation/css/normalize.css',
-    'lib/foundation/css/foundation.css',
-    'lib/foundation/js/foundation.js',
-    'lib/foundation/js/foundation.abide.js',
-    'lib/foundation/js/foundation.accordion.js',
-    'lib/foundation/js/foundation.alert.js',
-    'lib/foundation/js/foundation.clearing.js',
-    'lib/foundation/js/foundation.dropdown.js',
-    'lib/foundation/js/foundation.interchange.js',
-    'lib/foundation/js/foundation.joyride.js',
-    'lib/foundation/js/foundation.magellan.js',
-    'lib/foundation/js/foundation.offcanvas.js',
-    'lib/foundation/js/foundation.orbit.js',
-    'lib/foundation/js/foundation.reveal.js',
-    'lib/foundation/js/foundation.tab.js',
-    'lib/foundation/js/foundation.tooltip.js',
-    'lib/foundation/js/foundation.topbar.js',
-    'css/style.less',
-    'blocks/four_column/four_column.html',
-    'blocks/six_column/six_column.html',
-    'blocks/three_column/three_column.html',
-    'blocks/twelve_column/twelve_column.html',
-    'blocks/two_column/two_column.html',
-    'pages/home_page/home_page.html',
-    'pages/page_default/page_default.html',
-    'pages/sidebar_left/sidebar_left.html',
-    'pages/sidebar_right/sidebar_right.html',
-    'views/404.html',
-    'views/block_display.html',
-    'views/footer.html',
-    'views/header.html',
-    'views/layout.html',
-    'views/not_authorized.html',
-    'views/accounts/account_buttons.html',
-    'views/accounts/error.html',
-    'views/accounts/forgot_password.html',
-    'views/accounts/login.html',
-    'views/accounts/sign_up.html',
-    'views/accounts/social.html',
-    'lib/utils.js'
-  ], 'client');
+  api.add_files('lib/foundation/css/normalize.css', 'client');
+  api.add_files('lib/foundation/css/foundation.css', 'client');
+  api.add_files('lib/foundation/js/foundation.js', 'client');
+  api.add_files('lib/foundation/js/foundation.abide.js', 'client');
+  api.add_files('lib/foundation/js/foundation.accordion.js', 'client');
+  api.add_files('lib/foundation/js/foundation.alert.js', 'client');
+  api.add_files('lib/foundation/js/foundation.clearing.js', 'client');
+  api.add_files('lib/foundation/js/foundation.dropdown.js', 'client');
+  api.add_files('lib/foundation/js/foundation.interchange.js', 'client');
+  api.add_files('lib/foundation/js/foundation.joyride.js', 'client');
+  api.add_files('lib/foundation/js/foundation.magellan.js', 'client');
+  api.add_files('lib/foundation/js/foundation.offcanvas.js', 'client');
+  api.add_files('lib/foundation/js/foundation.orbit.js', 'client');
+  api.add_files('lib/foundation/js/foundation.reveal.js', 'client');
+  api.add_files('lib/foundation/js/foundation.tab.js', 'client');
+  api.add_files('lib/foundation/js/foundation.tooltip.js', 'client');
+  api.add_files('lib/foundation/js/foundation.topbar.js', 'client');
+  api.add_files('css/style.less', 'client');
+  api.add_files('blocks/four_column/four_column.html', 'client');
+  api.add_files('blocks/six_column/six_column.html', 'client');
+  api.add_files('blocks/three_column/three_column.html', 'client');
+  api.add_files('blocks/twelve_column/twelve_column.html', 'client');
+  api.add_files('blocks/two_column/two_column.html', 'client');
+  api.add_files('pages/home_page/home_page.html', 'client');
+  api.add_files('pages/page_default/page_default.html', 'client');
+  api.add_files('pages/sidebar_left/sidebar_left.html', 'client');
+  api.add_files('pages/sidebar_right/sidebar_right.html', 'client');
+  api.add_files('views/404.html', 'client');
+  api.add_files('views/block_display.html', 'client');
+  api.add_files('views/footer.html', 'client');
+  api.add_files('views/header.html', 'client');
+  api.add_files('views/layout.html', 'client');
+  api.add_files('views/not_authorized.html', 'client');
+  api.add_files('views/accounts/account_buttons.html', 'client');
+  api.add_files('views/accounts/error.html', 'client');
+  api.add_files('views/accounts/forgot_password.html', 'client');
+  api.add_files('views/accounts/login.html', 'client');
+  api.add_files('views/accounts/sign_up.html', 'client');
+  api.add_files('views/accounts/social.html', 'client');
+  api.add_files('lib/utils.js', 'client');
 });
-
